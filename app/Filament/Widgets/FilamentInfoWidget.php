68b28ac7cb022827e65f42546b77df44078847a4
Harvey Mendoza <harveymendoza2748@gmail.com>
2026-01-16 09:46:17 +0800

    add widget

---
+++ app/Filament/Widgets/FilamentInfoWidget.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use Filament\Widgets\Widget;
+
+class FilamentInfoWidget extends Widget
+{
+    protected string $view = 'filament.widgets.filament-info-widget';
+}
---
+++ resources/views/filament/widgets/filament-info-widget.blade.php
@@ -0,0 +1,18 @@
+<x-filament-widgets::widget>
+    <x-filament::section>
+            <div class="flex items-center gap-x-3">
+                <div class="flex-1">
+                    <a href="#" class="text-2xl font-bold tracking-tight system-title text-primary-500">
+                        National Administrative Register Online                    
+                    </a>
+                    <p class="text-xs text-gray-500 dark:text-gray-400">
+                        UPLC-Office of the National Administrative Register                    
+                    </p>
+                </div>
+
+            <div class="flex flex-col items-end gap-y-1">
+                
+            </div>
+        </div>
+    </x-filament::section>
+</x-filament-widgets::widget>
